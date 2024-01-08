5d0e387805e032469046302a4aa89d289ff07bc5
forhad-ds <106908358+forhad-ds@users.noreply.github.com>
2024-01-08 12:19:02 +0600

    Delete Function/util.R
    abcd

--- Function/util.R
+++
@@ -1,6 +0,0 @@
-setwd("C:/Users/forhad/Desktop/packg/Np") #set the working directory to the path of the packages
-pkgs <- list.files()
-
-install.packages(c(print(as.character(pkgs),
-                         collapse = "\",\"")),
-                 type = 'source', repos = NULL)
